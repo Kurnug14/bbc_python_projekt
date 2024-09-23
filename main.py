1c9066131cd8f1ec60329f5a85e66c51db78c1fc
Binz Natascha <natascha.binz1@bbcag.ch>
2024-09-23 17:05:50 +0200

    segmented the flashcard function

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import tkinter as tk
 
 import json_functions
 from tkinter.filedialog import askopenfilename
-
+current_set = None
+root = None
 cancel_button = False
 
 def cancel ():
@@ -18,19 +19,19 @@ def center_window(window, width, height):
     y_coordinate = int((screen_height / 2) - (height / 2))
     window.geometry(f"{width}x{height}+{x_coordinate}+{y_coordinate}")
 
-def correct_answer(answer_window):
+def correct_answer(answer_window, card):
     card_data = current_set.load_json(card)
     card_data['Level'] += 1
     current_set.mod_json(card, card_data['Level'])
     answer_window.destroy()
 
-def false_answer(answer_window):
+def false_answer(answer_window, card):
     card_data = current_set.load_json(card)
     card_data['Level'] = 1
     current_set.mod_json(card, card_data['Level'])
     answer_window.destroy()
 # Function to show the answer window
-def show_answer():
+def show_answer(card):
     #Close and load the window
     root.destroy()
     card_data = current_set.load_json(card)
@@ -44,20 +45,20 @@ def show_answer():
     button_frame = tk.Frame(answer_window)
     button_frame.pack(pady=10)
     korrekt_button = tk.Button(button_frame, text="Korrekt", font=("Arial", 12),
-                               command=lambda: correct_answer(answer_window))
+                               command=lambda: correct_answer(answer_window, card))
     korrekt_button.pack(side=tk.LEFT, padx=10)
     falsch_button = tk.Button(button_frame, text="Falsch", font=("Arial", 12),
-                              command=lambda: false_answer(answer_window))
+                              command=lambda: false_answer(answer_window, card))
     falsch_button.pack(side=tk.LEFT, padx=10)
 
 # Function to show the question
-def show_question():
+def show_question(card):
     card_data = current_set.load_json(card)
     root.title("Flashcard - Question")
     center_window(root, 400,200)
     question_label = tk.Label(root, text=f"Frage: {card_data['Frage']}", font=("Arial", 12), wraplength=300)
     question_label.pack(pady=20)
-    show_answer_button = tk.Button(root, text="Zeige Antwort", command=show_answer, font=("Arial", 12))
+    show_answer_button = tk.Button(root, text="Zeige Antwort", command=lambda: show_answer(card), font=("Arial", 12) )
     show_answer_button.pack(pady=10)
     close_button = tk.Button(root, text="Abbrechen", command=lambda: cancel(), font=("Arial", 12))
     close_button.pack(pady=10)
@@ -70,13 +71,16 @@ def choose_set():
                              filetypes=(("JSON files", "*.json*"), ("All Files", "*.*")))
     return(f_path)
 
-# Main window
-
-current_set = json_functions.ActiveSet(choose_set())
-cards = current_set.randomized_dic()
-for card in current_set.randomized_dic():
-    root = tk.Tk()
-    show_question()
-    if cancel_button:
-        cancel_button = False
-        break
+# Flashcard learning loop
+def flashcard_loop():
+    global current_set
+    global root
+    global cancel_button
+    current_set = json_functions.ActiveSet(choose_set())
+    for card in current_set.randomized_dic():
+        root = tk.Tk()
+        show_question(card)
+        if cancel_button:
+            cancel_button = False
+            break
+flashcard_loop()
